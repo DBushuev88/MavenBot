9f614e3d0903e998135155482773f648bac419ce
Dmitry.Bushuev <dmitry.bushuyev@stoloto.ru>
2020-09-28 16:30:31 +0300

    delete toolkas snapshopts, add telegrambots snapshot

--- Stobot/src/main/java/main.java
+++ Stobot/src/main/java/main.java
@@ -1,5 +1,3 @@
-//package ru.blogspot.toolkas.telegram.bot.echo;
-
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
